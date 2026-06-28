d8898b40c4a82efab5be10aae9a2dff652a22f83
FernandoCazco <fernando.cazco.guazhima@udla.edu.ec>
2026-06-28 17:54:33 -0500

    prototipos de funciones y funcion main con menu interactivo

--- RC2.c
+++ RC2.c
@@ -31,3 +31,75 @@ typedef struct {
     float viento;
     float humedad;
 } Zona;
+
+void   inicializar_zonas(Zona zonas[], int n);
+void   capturar_datos_actuales(Zona *z);
+void   capturar_climaticos(Zona *z);
+void   calcular_prediccion(Zona *z, Muestra *pred);
+void   calcular_promedios(Zona *z, Muestra *prom);
+void   emitir_alertas(Zona zonas[], int n);
+void   mostrar_recomendaciones(Zona *z, Muestra *pred);
+void   guardar_historico(Zona zonas[], int n);
+void   cargar_historico(Zona zonas[], int n);
+void   exportar_reporte(Zona zonas[], int n);
+void   mostrar_menu(void);
+void   mostrar_estado_actual(Zona zonas[], int n);
+void   mostrar_predicciones(Zona zonas[], int n);
+int    supera_limite(Muestra *m);
+float  promedio_ponderado(float datos[], int n);
+void   limpiar_buffer(void);
+
+int main(void) {
+    Zona   zonas[MAX_ZONAS];
+    int    opcion, i;
+
+    inicializar_zonas(zonas, MAX_ZONAS);
+    cargar_historico(zonas, MAX_ZONAS);
+
+    printf("\n=== SISTEMA INTEGRAL DE GESTIÓN DE CONTAMINACIÓN DEL AIRE ===\n\n");
+
+    do {
+        mostrar_menu();
+        printf("Opción: ");
+        scanf("%d", &opcion);
+        limpiar_buffer();
+
+        switch (opcion) {
+            case 1:
+                for (i = 0; i < MAX_ZONAS; i++) {
+                    printf("\n--- Ingreso de datos: %s ---\n", zonas[i].nombre);
+                    capturar_datos_actuales(&zonas[i]);
+                    capturar_climaticos(&zonas[i]);
+                }
+                guardar_historico(zonas, MAX_ZONAS);
+                printf("\nDatos guardados correctamente.\n");
+                break;
+
+            case 2:
+                mostrar_estado_actual(zonas, MAX_ZONAS);
+                break;
+
+            case 3:
+                mostrar_predicciones(zonas, MAX_ZONAS);
+                break;
+
+            case 4:
+                emitir_alertas(zonas, MAX_ZONAS);
+                break;
+
+            case 5:
+                exportar_reporte(zonas, MAX_ZONAS);
+                printf("\nReporte exportado a '%s'.\n", ARCHIVO_REPORTE);
+                break;
+
+            case 0:
+                printf("\nSistema cerrado. ¡Hasta pronto!\n");
+                break;
+
+            default:
+                printf("Opción inválida.\n");
+        }
+    } while (opcion != 0);
+
+    return 0;
+}
